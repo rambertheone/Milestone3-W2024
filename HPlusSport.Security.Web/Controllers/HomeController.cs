c7b1f5caef00b6b8692097b9798d5a57186ae57f
rambertheone <123188440+rambertheone@users.noreply.github.com>
2024-11-24 19:56:36 -0500

    corrected the error handling

--- HPlusSport.Security.Web/Controllers/HomeController.cs
+++ HPlusSport.Security.Web/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ public class HomeController : Controller
 {
     public IActionResult Index()
     {
+        var x = 1 / new Random().Next(0, 1);
         return View();
     }
 
--- HPlusSport.Security.Web/Program.cs
+++ HPlusSport.Security.Web/Program.cs
@@ -1,3 +1,4 @@
+
 using HPlusSport.Security.Web.Classes;
 using HPlusSport.Security.Web.Models;
 using Microsoft.AspNetCore.Authentication.Cookies;
@@ -5,16 +6,31 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddHsts(options =>
+{
+    options.MaxAge = TimeSpan.FromDays(365);
+    options.IncludeSubDomains = true;
+});
 
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie(options =>
     {
         options.LoginPath = new PathString("/Account/Login");
+        options.Cookie.HttpOnly = true;
+        options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+        options.Cookie.SameSite = SameSiteMode.Strict;
     });
 
-builder.Services.AddSession();
+builder.Services.AddSession(options =>
+{
+    options.Cookie.HttpOnly = true;
+    options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+    options.Cookie.SameSite = SameSiteMode.Strict;
+});
 builder.Services.AddHttpContextAccessor();
 
 builder.Services.AddDbContext<ShopContext>(options =>
@@ -22,15 +38,20 @@ builder.Services.AddDbContext<ShopContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseExceptionHandler("/Home/Error");
+
+app.UseStatusCodePages();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();

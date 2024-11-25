ac093d814829b780fc9cf23d76f7d7e3a7004d54
rambertheone <123188440+rambertheone@users.noreply.github.com>
2024-11-24 19:11:37 -0500

    created the identification pages using scaffolding

---
+++ HPlusSport.Security/Areas/Identity/Data/HPlusSportSecurityContext.cs
@@ -0,0 +1,22 @@
+﻿using HPlusSport.Security.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HPlusSport.Security.Data;
+
+public class HPlusSportSecurityContext : IdentityDbContext<HPlusSportSecurityUser>
+{
+    public HPlusSportSecurityContext(DbContextOptions<HPlusSportSecurityContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
